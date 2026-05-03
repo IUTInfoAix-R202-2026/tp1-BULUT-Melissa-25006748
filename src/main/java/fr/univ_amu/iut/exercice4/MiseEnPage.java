29fdd89e9fdf22e7a4bcb731e8cb159c81e9d2e0
BULUT-Melissa-25006748 <melissa.bulut@etu.univ-amu.fr>
2026-05-03 19:47:54 +0000

    Exercice 4 terminé

--- src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
+++ src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
@@ -1,6 +1,15 @@
 package fr.univ_amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -44,6 +53,31 @@ public class MiseEnPage extends Application {
     // Donne un id CSS utile sur les composants si tu veux les retrouver
     // facilement (les tests utilisent lookup sur les classes ".text-field"
     // et ".button", pas besoin d'id particulier).
+
+    BorderPane borderPane = new BorderPane();
+    Scene s = new Scene(borderPane);
+
+    GridPane gridpane = new GridPane();
+    gridpane.add(new Label("Nom :"), 0, 0);
+    gridpane.add(new Label("Email :"), 0, 1);
+    gridpane.add(new TextField(), 1, 0);
+    gridpane.add(new TextField(), 1, 1);
+    borderPane.setCenter(gridpane);
+
+    Menu menu1 = new Menu("Fichier");
+    Menu menu2 = new Menu("Aide");
+
+    MenuBar menuBar = new MenuBar(menu1, menu2);
+    borderPane.setTop(menuBar);
+
+    HBox hbox = new HBox();
+    Button button1 = new Button("Valider");
+    Button button2 = new Button("Annuler");
+    hbox.getChildren().addAll(button1, button2);
+    borderPane.setBottom(hbox);
+
+    primaryStage.setScene(s);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
